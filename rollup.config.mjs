26bb2e0c868eae4b72a0ca0451bf5b8adb57fe31
pinkfish <pinkfish@users.noreply.github.com>
2024-06-05 10:55:17 -0700

    Fix code to only generate one file in the dist model

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,20 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
-import serve from 'rollup-plugin-serve';
 import json from '@rollup/plugin-json';
 
 const dev = process.env.ROLLUP_WATCH;
-
-const serveopts = {
-  contentBase: ['./dist'],
-  host: '0.0.0.0',
-  port: 5000,
-  allowCrossOrigin: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
-};
 
 const plugins = [
   nodeResolve({}),
@@ -27,16 +16,18 @@ const plugins = [
     exclude: 'node_modules/**',
     babelHelpers: 'bundled',
   }),
-  dev && serve(serveopts),
-  !dev && terser(),
+  terser(),
 ];
 
 export default [
   {
     input: 'src/simply-magic-area-card.ts',
     output: {
-      dir: 'dist',
+      inlineDynamicImports: true,
+      esModule: true,
+      file: 'dist/simply-magic-area-card.js',
       format: 'es',
+      sourcemap: true,
     },
     plugins: [...plugins],
   },
